19840ee39d5168a85202bb12ec11f2116da9be6b
Promss <tima.nuranbekov@gmail.com>
2023-03-01 12:50:09 +0600

    feat: add authorizationErrorScreen

---
+++ lib/authorizationErrorScreen.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class AuthorizationError extends StatelessWidget {
+  const AuthorizationError({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+    var height = size.height;
+    var width = size.width;
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: width / 22),
+          child: Column(children: [
+            Padding(
+              padding: EdgeInsets.only(top: height / 12, bottom: height / 20),
+              child: Column(
+                children: [
+                  Text(
+                    'Ошибка авторизации',
+                    style: GoogleFonts.inter(
+                        fontSize: height / 35, fontWeight: FontWeight.w600),
+                  ),
+                  SizedBox(
+                    height: height / 45,
+                  ),
+                  Text(
+                    'Попробуйте проверить свое интернет соединение и войти в систему через несколько минут.',
+                    style: GoogleFonts.inter(
+                        fontSize: height / 45, fontWeight: FontWeight.w400),
+                    textAlign: TextAlign.center,
+                  ),
+                ],
+              ),
+            ),
+            InkWell(
+              onTap: (() {
+                Navigator.pushNamed(context, 'auth');
+              }),
+              child: Container(
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(8),
+                    color: Color.fromRGBO(137, 115, 198, 1)),
+                alignment: Alignment.center,
+                height: height / 15,
+                width: width,
+                child: Text('Хорошо',
+                    style: GoogleFonts.inter(
+                        fontSize: height / 50,
+                        fontWeight: FontWeight.w600,
+                        color: Colors.white)),
+              ),
+            ),
+          ]),
+        ),
+      ),
+    );
+  }
+}
